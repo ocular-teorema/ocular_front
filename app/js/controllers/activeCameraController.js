c9ddff5b8c0b0be63daa14c28855679e4ae7f296
Vahivsky Slava <vahivsky@gmail.com>
2019-07-26 12:58:29 +0300

    fix for active camera

--- app/js/controllers/activeCameraController.js
+++ app/js/controllers/activeCameraController.js
@@ -19,9 +19,17 @@ module.controller('activeCameraController', function($scope, camerasList) {
         var cameraId = Number(eventData.cam_id);
         if (cameraId) {
             var camera = camerasList.data.filter(function (c) { return c.id === cameraId; })[0];
+            console.log('Camera:', camera);
             if (camera) {
-                $scipe.ws_video_url = camera.ws_video_url;
+                $scope.ws_video_url = camera.ws_video_url;
+                console.log('Url:', $scope.ws_video_url);
+                $scope.apply();
             }
         }
+    }
+
+    $scope.test = function () {
+        console.log('Cameras: ', camerasList.data);
+        onActiveCameraChanged({cam_id: camerasList.data[0].id});
     }
 });
